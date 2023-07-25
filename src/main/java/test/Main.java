98de729f3b6ee3a5bb096cafab541c467c33a957
Hanwen123456 <hw020118c>
2023-07-25 15:03:23 +0800

    init:修改项目

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args){
         JiSuan suan = new JiSuan();
         int n = suan.add(10,5);
-        System.out.println(n);
+        System.out.println("计算的结果"+n);
+
     }
 }
